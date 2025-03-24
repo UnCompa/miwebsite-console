f9b51e5374e5bb6c9298f83023c29abc08d19f34
josuetipan <josuetipan101@gmail.com>
2025-03-24 17:20:10 -0500

    change fix screen login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -43,12 +43,12 @@ function LoginPage() {
   });
 
   return (
-    <main className="min-h-screen flex justify-center items-center">
+    <main className="min-h-screen flex justify-center items-center bg-bl">
       <Toaster theme="dark" richColors />
-      <div className="flex bg-neutral-900 text-white rounded-3xl shadow-2xl overflow-hidden w-[900px]">
+      <div className="flex bg-amber-900 text-white rounded-3xl shadow-2xl  w-full  justify-center ">
         {/* Left Side - Image & Logos */}
-        <div className="flex flex-col items-center  p-100 px-0 py-0">
-          <div className="relative flex flex-col items-center bg-black">
+        <div className="flex flex-col items-center bg-black p-100 px-0 py-0 h-screen w-[100%]">
+          <div className="relative flex flex-col items-center bg-red-700">
             <img
               src={astronautCat}
               alt="Astronaut Cat"
@@ -69,52 +69,55 @@ function LoginPage() {
         </div>
 
         {/* Right Side - Login Form */}
-        <section className="w-1/2 p-8 flex flex-col justify-center">
-          <div className="flex items-center justify-center mb-4">
-            <img src={logo} alt="UnCompa.Dev" className="w-24" />
-            <img src={unCompa} className="p-5" />
+        <section className=" flex flex-col justify-start bg-[#0C1327] w-[60%] ">
+          <div className="flex items-center justify-start pl-3 mb-4 bg-[#020617] py-3">
+            <img src={logo} alt="UnCompa.Dev" className="w-15" />
+            <img src={unCompa} className="w-2/4 pl-2" />
           </div>
-          <h1 className="text-3xl font-semibold text-center">Bienvenido</h1>
-          <p className="text-sm text-gray-400 text-center mb-6">
-            Por favor ingresa tus datos
-          </p>
-          <form onSubmit={formik.handleSubmit} className="space-y-4">
-            <div className="flex items-center gap-2 bg-neutral-800 rounded-lg px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-400/40">
-              <IoPerson className="text-cyan-400" />
-              <input
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                name="username"
-                type="text"
-                className="bg-transparent flex-1 outline-none text-white placeholder-gray-400"
-                placeholder="Nombre de usuario"
-              />
-            </div>
-            <div className="flex items-center gap-2 bg-neutral-800 rounded-lg px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-400/40">
-              <IoKey className="text-cyan-400" />
-              <input
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                name="password"
-                type={showPassword ? "text" : "password"}
-                className="bg-transparent flex-1 outline-none text-white placeholder-gray-400"
-                placeholder="Contraseña"
-              />
+          <div className="p-15 flex  flex-col justify-start ">
+            <h1 className="text-3xl font-semibold ">Bienvenido</h1>
+            <p className="text-sm text-gray-400 mb-6">
+              Por favor ingresa tus datos
+            </p>
+            <p className="text-sm text-white pt-1 mb-1">Nombre de usuario</p>
+            <form onSubmit={formik.handleSubmit} className="space-y-8  ">
+              <div className="flex items-center gap-2 bg-neutral-800 rounded-lg px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-400/40">
+                <IoPerson className="text-cyan-400" />
+                <input
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  name="username"
+                  type="text"
+                  className="bg-transparent flex-1 outline-none text-white placeholder-gray-400"
+                  placeholder="Nombre de usuario"
+                />
+              </div>
+              <div className="flex items-center gap-2 bg-neutral-800 rounded-lg px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-400/40">
+                <IoKey className="text-cyan-400" />
+                <input
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  className="bg-transparent flex-1 outline-none text-white placeholder-gray-400"
+                  placeholder="Contraseña"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="text-cyan-400"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
               <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="text-cyan-400"
+                type="submit"
+                className="w-full bg-blue-600 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition-all"
               >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                {isLoading ? <LoadingSpinner /> : "Enviar"}
               </button>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-600 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition-all"
-            >
-              {isLoading ? <LoadingSpinner /> : "Enviar"}
-            </button>
-          </form>
+            </form>
+          </div>
         </section>
       </div>
     </main>
