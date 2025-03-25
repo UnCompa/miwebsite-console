de1bd9bbe3d53a0902a4cbb5dcf77e883b065c63
UnCompa <brandonddxd@gmail.com>
2025-03-25 07:24:03 -0500

    feat :sparkes: adding new login page (josuetipan101)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,17 +4,17 @@ import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { IoKey, IoPerson } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { toast, Toaster } from "sonner";
-import useAuthStore from "../store/useAuthStore";
-import "../styles/pattern.css";
-import logo from "../assets/logo.png";
-import unCompa from "../assets/UnCompa.Dev.png";
 import astronautCat from "../assets/astronaut-cat.png";
 import html5Logo from "../assets/html5.png";
-import reactLogo from "../assets/react.png";
 import jsLogo from "../assets/javascript.png";
+import logo from "../assets/logo.png";
 import nestLogo from "../assets/nest.png";
+import reactLogo from "../assets/react.png";
 import tailwindLogo from "../assets/tailwind.png";
+import unCompa from "../assets/UnCompa.Dev.png";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
+import useAuthStore from "../store/useAuthStore";
+import "../styles/pattern.css";
 
 function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
@@ -45,9 +45,9 @@ function LoginPage() {
   return (
     <main className="min-h-screen flex justify-center items-center bg-bl">
       <Toaster theme="dark" richColors />
-      <div className="flex bg-amber-900 text-white rounded-3xl shadow-2xl  w-full  justify-center ">
+      <div className="flex text-white rounded-3xl shadow-2xl  w-full  justify-center ">
         {/* Left Side - Image & Logos */}
-        <div className="flex flex-col items-center bg-black p-100 px-0 py-0 h-screen w-[100%]">
+        <div className="hidden md:flex flex-col items-center bg-black p-100 px-0 py-0 h-screen w-[100%]">
           <div className="relative flex flex-col items-center bg-red-700">
             <img
               src={astronautCat}
@@ -69,7 +69,7 @@ function LoginPage() {
         </div>
 
         {/* Right Side - Login Form */}
-        <section className=" flex flex-col justify-start bg-[#0C1327] w-[60%] ">
+        <section className=" flex flex-col justify-start bg-[#0C1327] w-full sm:w-[60%] ">
           <div className="flex items-center justify-start pl-3 mb-4 bg-[#020617] py-3">
             <img src={logo} alt="UnCompa.Dev" className="w-15" />
             <img src={unCompa} className="w-2/4 pl-2" />
